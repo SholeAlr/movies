e2bead836eff30a27701dba0778bb1eb10c3b483
sholealirezaei <sh.alirezaii@gmail.com>
2024-10-04 16:41:19 +0330

    add menu icons and toggle functionality to them

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,16 @@
 import { Header } from './Header'
 import { Sidebar } from './Sidebar'
 import { Main } from './Main'
+import { useState } from 'react'
 
 export const AppLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
   return (
     <div className='h-screen w-screen'>
-      <Header />
+      <Header
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Sidebar />
       <Main />
     </div>
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,22 @@
-import { HambergerMenu } from 'iconsax-react'
-import React from 'react'
+import { Add, HambergerMenu } from 'iconsax-react'
+import React, { FC } from 'react'
+import { HeaderProps } from './@types/header.type'
 
-export const Header = () => {
+export const Header: FC<HeaderProps> = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
+}) => {
+  const toggleSidebarClick = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
   return (
     <div className='h-10 bg-slate-900 p-2 fixed top-0 w-screen flex justify-between'>
       <h4>Movies & Artists</h4>
-      <HambergerMenu className='text-gray-300' />
+      {isSidebarOpen ? (
+        <Add className='text-gray-300 rotate-45' onClick={toggleSidebarClick} />
+      ) : (
+        <HambergerMenu className='text-gray-300' onClick={toggleSidebarClick} />
+      )}
     </div>
   )
 }
